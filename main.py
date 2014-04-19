5d7d1a2bb615707f8583f18ebd79dd86ecc9f15e
Wenley Tong <wenley@squareup.com>
2014-04-19 15:35:34 -0400

    Intermediate first draft of python endponit

---
+++ main.py
@@ -0,0 +1,75 @@
+
+# Requires you to `pip install python-gnupg`
+
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+from gnupg import GPG
+gpg = GPG()
+
+import json
+
+@app.route("/metaphors/")
+def metaphorIndex():
+  with open('metaphor_index.html') as f:
+    s = f.read()
+  return s
+
+@app.route("/start_send_task")
+def inital_data_for_sending():
+  return json.dumps({
+    'alice': gpg.alice,
+    'bob': gpg.bob,
+    'message': "Hey Alice, want to talk secretly at the secret place at 10 pm tonight?",
+    })
+
+@app.route("/start_verify_message")
+def inital_data_for_verifying():
+  message = "Hey, the NSA is onto us. Get packed and meet me at the drop zone."
+  encrypted = gpg.encrypt(message, gpg.me)
+
+@app.route("/finished")
+def finish_task():
+  logs = request.form['logs']
+  return "OK"
+
+@app.route("/action", methods=["POST"])
+def do_action():
+  fingerprint = gpg.keys[request.form['key_index']]
+  action = request.form['action']
+  if action == 'lock':
+    pass
+  elif action == 'sign':
+    pass
+  elif action == 'unlock':
+    pass
+  elif action == 'verify':
+    pass
+  else:
+    return "unrecognized action. Please restart and try again."
+
+@app.route('/new_key', methods=["POST"])
+def choose_new_key():
+  '''
+  Expects POST with list field 'taken_keys' that
+  contains the fingerprints of the keys that are
+  already taken.
+  '''
+  taken_keys = request.form['taken_keys']
+
+def initialize_gpg():
+  num_keys = len(gpg.list_keys())
+  for i in xrange(num_keys, 50):
+    key_input = gpg.gen_key_input(key_type="RSA", key_length=2048)
+    key = gpg.gen_key(key_input)
+
+  # Adds field for quick access to fingerprints
+  gpg.keys = [k['fingerprint'] for k in gpg.list_keys(True)]
+  gpg.alice = keys[0]
+  gpg.bob = keys[1]
+  gpg.me = keys[2]
+
+if __name__ == "__main__":
+  initialize_gpg()
+  app.run()
